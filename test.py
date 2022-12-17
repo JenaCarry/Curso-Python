584b767e8d495d7aaf70ef9cd61cbfaef03a3032
Jean <120676537+JenaCarry@users.noreply.github.com>
2022-12-17 03:07:23 -0300

    Update test.py

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ Principais vantagens:
 Usuário - repositório local = Git - push - repositório remoto = GitHub
 
 GitHub - Plataforma de Rede Social para Programadores
+
 Principais vantagens:
 - Repositórios ilimitados
 - Hospedagem de código-fonte
@@ -23,13 +24,42 @@ Principais vantagens:
 - GitHub Pages integrado
 - Colaboração
 - Forks - Pegar um projeto
+
 https://octodex.github.com/ - site do mascotes GitHub personalizados
+
 - Issues - perguntar ou ajudar
 - Markdown ou .md / Onde usar:
 1º README
 2º Issues
 3º Pull request
-* colocar em itálico
-** ou __ colocar em negrito
-Saved Replies
+
+- Aprendendo Markdown
+Para colocar em itálico  * ou _
+Para colocar em negrito ** ou __
+Para riscar a palavra ao meio ~~
+Para criar um título # nível 1
+Para criar um título ## nível 2
+Para criar um título ### nível 3
+Para adicionar uma linha em baixo do título --- ou ***
+Lista numerada:
+Adicionando 1. para número normal ou três espaços 1. para número romano
+Lista demarcada:
+Adicionando * ou - para bolinha ou três espaços * para bolinha aberta
+Lista de tarefas:
+- [] "Tarefa a fazer"
+- [x] "Tarefa concluída"
+Criando uma Tabela:
+Num | Nome | Nota
+---|---|---
+1 | Jean | 8.5
+Fim Tabela
+
+Crase `document.getElementById()` mostra o conteúdo destacado
+Três ```CÓDIGO``` mostra o conteúdo destacado
+
+Colocando emojis : dois pontos e o nome do emoji
+
+
+
+
 '''
